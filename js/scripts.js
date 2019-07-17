b933ea5ba72bbb801a226f65ef3c0e697c53efa7
irahozadenyse <irahodeny1@gmail.com>
2019-07-17 14:52:41 +0200

    added some code in html and js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var dog =true;
+var cat =true;
+alert("Which pet should you get?")
+if("dog"){
+    console.log("the dog is the one")
+}
+else if("cat"){
+    console.log("the cat is the one")
+}
+else if("cat,dog"){
+    console.log("both")
+}
+else{
+    console.log("neither of those")
+}
+dog;
+cat;
+
+
+
+
